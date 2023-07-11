3370b25be886e2f23d2e53f78165382268fa15d8
AkiraZeZero <kiara.ocasio07@gmail.com>
2023-07-11 17:26:36 -0400

    skills box/grid organized

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,82 +1,100 @@
-import { Container, Box, Stack} from "@mui/system";
-import Divider from '@mui/material/Divider';
-import Chip from '@mui/material/Chip';
+import { Container, Box, Stack } from "@mui/system";
+import Divider from "@mui/material/Divider";
+import MuiGrid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
 
 const Skills = () => {
-    const skills = (<div>{`My skills bitch`}</div>)
+  const Grid = styled(MuiGrid)(({ theme }) => ({
+    width: "100%",
+    ...theme.typography.body2,
+    '& [role="separator"]': {
+      margin: theme.spacing(0, 2),
+    },
+  }));
   return (
-    <Stack direction="row">
-        <Chip label="Skills"/>
-{skills}
-    <Divider>LEFT</Divider>
-    <Divider>LEFT</Divider>
-    <Box className="skillsBox">
-      <Container
-        className="skillsContainer"
-        maxWidth="md"
-        sx={{
-          borderRadius: "16px",
-        }}
-      >
+    <div>
+      <Grid container></Grid>
+      <Box className="skillsBox">
+        <Container
+          className="skillsContainer"
+          maxWidth="sm"
+          sx={{
+            borderRadius: "16px",
+          }}
+        >
           <h3>Skills!</h3>
-        <Box>
-          <p>
-            Languages:
-            <li>JavaScript</li>
-            <li>ES6</li>
-          </p>
-          <p>Databases:
-            <li>SQL</li>
-            <li>MySql</li>
-            <li>Heroku</li>
-            <li>Postman</li>
-          </p>
-          <p>Front End:
-            <li>HTML/HTML5</li>
-            <li>ReactJS</li>
-            <li>Redux</li>
-            <li>React Native</li>
-            <li>Typescript</li>
-            <li>ESS/ES6</li>
-            <li>JSON</li>
-            <li>AJAX</li>
-            <li>MaterialUI</li>
-            <li>Bootstrap</li>
-            <li>CSS/CSS3</li>
-            <li>LESS</li>
-            <li>SASS</li>
-          </p>
-          <p>Back End:
-            <li>Node.js</li>
-          </p>
-        </Box>
-        <Box>
-            <p>Testing:
-                <li>Jest</li>
-                <li>Cypress</li>
-            </p>
-            <p>Package Control:
-                <li>Git</li>
-                <li>Github</li>
-            </p>
-            <p>Methodologies:
-                <li>Agile</li>
-                <li>Waterfall</li>
-                <li>SDLC</li>
-            </p>
-            <p>Other:
-                <li>AI/Chat GPT</li>
-                <li>Google</li>
-                <li>Adobe Photoshop</li>
-                <li>Wireframing</li>
-                <li>Fast Learner/Adaptive</li>
-                <li>Team Player</li>
-                <li>Great Humor ( ˙▿˙ )b</li>
-            </p>
-        </Box>
-      </Container>
-    </Box>
-    </Stack>
+          <Stack direction={"row"}>
+            <Grid>
+              <Box sx={{ textAlign: "start" }}>
+                <p>
+                  <Divider>Languages</Divider>
+                  <li>JavaScript</li>
+                  <li>ES6</li>
+                </p>
+                <p>
+                  <Divider>Databases</Divider>
+                  <li>SQL</li>
+                  <li>MySql</li>
+                  <li>Heroku</li>
+                  <li>Postman</li>
+                </p>
+                <p>
+                  <Divider>Front End</Divider>
+                  <li>HTML/HTML5</li>
+                  <li>ReactJS</li>
+                  <li>Redux</li>
+                  <li>React Native</li>
+                  <li>Typescript</li>
+                  <li>ESS/ES6</li>
+                  <li>JSON</li>
+                  <li>AJAX</li>
+                  <li>MaterialUI</li>
+                  <li>Bootstrap</li>
+                  <li>CSS/CSS3</li>
+                  <li>LESS</li>
+                  <li>SASS</li>
+                </p>
+              </Box>
+            </Grid>
+            <Divider orientation="vertical" flexItem></Divider>
+            <Grid>
+              <Box sx={{ textAlign: "start" }}>
+                <p>
+                  <Divider>Back End</Divider>
+                  <li>Node.js</li>
+                </p>
+                <p>
+                  <Divider>Testing</Divider>
+                  <li>Jest</li>
+                  <li>Cypress</li>
+                </p>
+                <p>
+                  <Divider>Package Control</Divider>
+                  <li>Git</li>
+                  <li>Github</li>
+                </p>
+                <p>
+                  <Divider>Methodologies</Divider>
+                  <li>Agile</li>
+                  <li>Waterfall</li>
+                  <li>SDLC</li>
+                </p>
+                <p>
+                  <Divider>Other</Divider>
+                  <li>AI/Chat GPT</li>
+                  <li>Google</li>
+                  <li>Adobe Photoshop</li>
+                  <li>Wireframing</li>
+                  <li>Fast Learner/Adaptive</li>
+                  <li>Team Player</li>
+                  <li>Great Humor ( ˙▿˙ )b</li>
+                </p>
+              </Box>
+            </Grid>
+          </Stack>
+        </Container>
+      </Box>
+    </div>
   );
 };
 // what if we had skills as a slider tho
--- src/Components/Testing.jsx
+++ src/Components/Testing.jsx
@@ -1,23 +1,40 @@
 import Akira from "../Images/Kiara.jpg";
-import { Container, Box } from "@mui/system";
+import { Container, Box, Stack } from "@mui/system";
 import { Avatar } from "@mui/material";
+import MuiGrid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
 
 const Kiara = () => {
+  const Grid = styled(MuiGrid)(({ theme }) => ({
+    width: '100%',
+    ...theme.typography.body2,
+    '& [role="separator"]': {
+      margin: theme.spacing(0, 2),
+    },
+  }));
   return (
+    <Grid container>
     <Box>
       <Container
         maxWidth="xs"
         className="kiaraSection"
         sx={{ borderRadius: "16px" }}
-        >
-        This is a test playground
-        <Avatar alt="Kiara" src={Akira} sx={{ width: 100, height: 100 }} />
-        <img className="kiaraPicture" src={Akira} alt="Kiara" />
-
-        <Box>a box w a container inside w a box inside</Box>
+      >
+        This is a test playground!
+        <Stack direction={"row"}>
+          <Avatar alt="Kiara" src={Akira} sx={{ width: 100, height: 100 }} />
+          <Container>container in stack</Container>
+          <Container>Another container in the stack</Container>
+        </Stack>
+        <Stack direction={"row"}>
+          <Container>Stack number 2</Container>
+          <Container>Another container stack</Container>
+          <Container>And the 3rd last stack item</Container>
+        </Stack>
+        <Box>a box outside of the stack</Box>
       </Container>
       <Container>a box w a container inside</Container>
-    </Box>
+    </Box></Grid>
   );
 };
 
